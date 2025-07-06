872295d87081d4989cc3173121b937b17ead645d
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2025-07-06 02:58:47 +0000

    add linux build script

---
+++ build-vntextpatch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# filepath: /workspaces/VNTextPatch-wasm/build-vntextpatch.sh
+
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+# Limpeza dos diretórios antigos
+find . -type d -name bin -exec rm -rf {} +
+find . -type d -name obj -exec rm -rf {} +
+rm -rf Build/VNTextPatch
+rm -rf Debug
+rm -rf Release
+
+mkdir -p Build
+
+# Restaura dependências
+dotnet restore VNTextPatch/VNTextPatch.csproj
+
+# Compila em Release para a pasta Build/VNTextPatch
+dotnet build VNTextPatch/VNTextPatch.csproj -c Release -o Build/VNTextPatch
+
+# Remove arquivos indesejados
+rm -f Build/VNTextPatch/*.pdb
+rm -f Build/VNTextPatch/*.txt
