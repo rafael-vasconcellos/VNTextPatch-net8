2ba822283133e04f2310b7f2f1c30fe0e26ae2f0
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2025-07-06 02:17:40 +0000

    Update build script

--- build-vntextpatch.bat
+++ build-vntextpatch.bat
@@ -2,6 +2,7 @@
 
 set DOTNET_CLI_TELEMETRY_OPTOUT=1
 
+REM Limpeza dos diretórios antigos
 for /F "tokens=*" %%f in ('dir /B /AD /S bin') do rmdir /S /Q "%%f"
 for /F "tokens=*" %%f in ('dir /B /AD /S obj') do rmdir /S /Q "%%f"
 if exist Build\VNTextPatch rmdir /S /Q Build\VNTextPatch
@@ -9,11 +10,14 @@ if exist Debug rmdir /S /Q Debug
 if exist Release rmdir /S /Q Release
 
 mkdir Build
-mkdir Build\VNTextPatch
 
-dotnet restore VNTextPatch\VNTextPatch.csproj /p:RuntimeIdentifiers=win
-msbuild VNTextPatch\VNTextPatch.csproj /p:LangVersion=9 /p:AllowUnsafeBlocks=true /p:Platform=AnyCPU /p:Configuration=Release /p:OutputPath=..\Build\VNTextPatch\
-#del Build\VNTextPatch\FreeMote*.xml
+REM Restaura dependências
+dotnet restore VNTextPatch\VNTextPatch.csproj
+
+REM Compila em Release para a pasta Build\VNTextPatch
+dotnet build VNTextPatch\VNTextPatch.csproj -c Release -o Build\VNTextPatch
+
+REM Remove arquivos indesejados
 del Build\VNTextPatch\*.pdb
 del Build\VNTextPatch\*.txt
 
